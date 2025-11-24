c74d773572065190825576b56c78f9f073a57810
Oleksandr <sov1620@gmail.com>
2025-11-24 23:06:46 +0200

    my eleventh js-hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+
+getImagesByQuery(query) {
+
+};
--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
 // import fetchData from `./js/pixabay-api`;
 // import ModuleName from `./js/render-functions`;
