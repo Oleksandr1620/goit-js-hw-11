8191c4d391df3cec2f5edb201b7fb2efd9834c6a
Oleksandr <sov1620@gmail.com>
2025-11-25 21:32:35 +0200

    my eleventh js-hw

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -13,11 +13,30 @@ const lightbox = new SimpleLightbox('.gallery a', {
 export function createGallery(images) {
   const markup = images
     .map(
-      ({ webformatURL, largeImageURL, tags }) => `
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
       <li class="gallery-item">
         <a href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+         <img src="${webformatURL}" alt="${tags
+        .split(',')
+        .slice(0, 3)
+        .join(', ')}" />
+
+
         </a>
+         <div class="info">
+          <p><b>Likes</b> ${likes}</p>
+          <p><b>Views</b> ${views}</p>
+          <p><b>Comments</b> ${comments}</p>
+          <p><b>Downloads</b> ${downloads}</p>
+        </div>
       </li>
     `
     )
